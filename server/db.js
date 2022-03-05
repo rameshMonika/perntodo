e702544c3407a6086849f15202571f788a84b023
rameshMonika <Monika.20@ichat.sp.edu.sg>
2022-03-05 19:50:17 +0800

    update a bit of changes

--- server/db.js
+++ server/db.js
@@ -2,12 +2,19 @@ const Pool =require("pg").Pool;
 
 const pool=new Pool(
    {
-       user :"postgres",
-       password:"akinom1811",
-       host:"localhost",
+       user :"vmjybvhx",
+       password:"sOCnaMOLZoix6Z8zzzVCaf2-sfjPICYd",
+       host:"jelani.db.elephantsql.com",
        port:5432,
-       database:'perntodo'
+       database:'vmjybvhx'
    }
 );
 
 module.exports=pool;
+
+
+// user :"postgres",
+// password:"akinom1811",
+// host:"localhost",
+// port:5432,
+// database:'perntodo'
